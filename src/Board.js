c966c125468a119706ed9ceabf87e84492d32f2d
Adrian Wiecheć <adrian.wiechec@optilyz.com>
2020-04-28 00:16:18 +0200

    Render something

--- src/Board.js
+++ src/Board.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const Board = ({ board, pyramids }) => <div className="grid grid-cols-8">
-  {board.map(({ row, column }) => {
-    const isEven = (column + row) % 2 ? 'bg-green-400' : 'bg-purple-400';
-    const id = `${row}-${column}`;
-    return <div key={id} className={`text-center ${isEven}`}>{id}</div>
-  })}
+const Board = ({ board, pyramids }) =>
+  <div className="p-2 bg-orange-500 grid grid-cols-8 gap-1">
+    {board.map(({ row, column }) => {
+      const isEven = (column + row) % 2 ? 'bg-green-400' : 'bg-purple-400';
+      const fieldId = `${row}-${column}`;
+      console.log({ fieldId, pyramids });
+      const { color, size, direction } = pyramids[fieldId] || {};
+      return <div key={fieldId} className={`text-center ${isEven}`}>{color}-{size}-{direction}</div>
+    })}
 
-</div>;
+  </div>;
 
 export default Board;
